c7f9750f585e751c4c18728e2af4a0aec204a784
Ivan Sanjaya <ivansanjaya11keluer@gmail.com>
2026-04-17 13:32:22 -0600

    fixed the issue when doing github actions and there is error traceback

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from src.code_review_with_llm.Controller import Controller
 from src.code_review_with_llm.Model import Model
 from src.code_review_with_llm.View import View
+import os
 
 if __name__ == "__main__":
-    controller = Controller()
-    model = Model(controller=controller)
-    view = View(controller)
+    if not os.getenv("CI"):
+        controller = Controller()
+        model = Model(controller=controller)
+        view = View(controller)
 
-    controller.set_model(model)
-    controller.set_view(view)
+        controller.set_model(model)
+        controller.set_view(view)
 
-    view.mainloop()
+        view.mainloop()
